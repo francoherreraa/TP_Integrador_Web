349132752f6102f715caeb539740f5bd71eddf67
iann1x <ianchiti308@gmail.com>
2024-10-24 22:56:39 -0300

    cambios boton dark mode

--- script.js
+++ script.js
@@ -55,30 +55,29 @@ function avanzarImgs(){
 
 /*<!boton dark mode--- >*/
 var toggle = document.getElementById('conteiner');
-var body = document.querySelector('body');
+var body = document.body;
 var sponsor = document.getElementById('patrocinio');
 var celdas = document.querySelectorAll('.celda');
 var subCabecera = document.querySelectorAll('.subCabecera');
 var ul = document.querySelector('ul');
 var secSub = document.querySelectorAll('.secSub');
 
-toggle.onclick = function() {
-    toggle.classList.toggle('active');
-    body.classList.toggle('active');
-    sponsor.classList.toggle('active');
-    
-    subCabecera.forEach(function(element) {
-        element.classList.toggle('active');
-    });
-    
-    secSub.forEach(function(letras) {
-        letras.classList.toggle('active');
-    });
+if (toggle) {
+    toggle.onclick = function() {
+        toggle.classList.toggle('active');
+        body.classList.toggle('active');
+        if (sponsor) sponsor.classList.toggle('active');
 
-    ul.classList.toggle('active');
+        subCabecera.forEach(function(element) {
+            element.classList.toggle('active');
+        });
+        secSub.forEach(function(element) {
+            element.classList.toggle('active');
+        });
+        if (ul) ul.classList.toggle('active');
 
-    celdas.forEach(function(celda) {
-        celda.classList.toggle('active');
-    });
-};
-
+        celdas.forEach(function(celda) {
+            celda.classList.toggle('active');
+        });
+    };
+}
